4fe30d73ce94f1dbf9d3f7f405be03c2eb6a47d9
Ting Lu <tinglu30@gmail.com>
2023-08-12 19:05:48 -0400

    Update Three art section

--- public/art copy.js
+++
@@ -1,8 +0,0 @@
-number_of_art = 15;
-let art ="";
-
-for (let i = 1; i <= number_of_art; i ++){
-  art += `<div class = "art-g"><img class = "art-g" src = "Art/art (${i}).jpg"></div>`;
-}
-
-document.querySelector(".art-container").innerHTML = art
