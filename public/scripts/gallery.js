1e8b0b70ecf51840d368f73b333d5b70d62feb94
Ting Lu <tinglu30@gmail.com>
2023-08-25 01:31:46 -0400

    Latest Version

--- public/scripts/gallery.js
+++ public/scripts/gallery.js
@@ -1,11 +1,12 @@
-number_of_art = 15;
-current_item = 10;
+let number_of_art = 15;
+let current_item = 10;
 let art ="";
-
+let grid = document.querySelector(".art-container");
 
 
 for (let i = 1; i <= current_item; i ++){
   art += `<div class = "art-g"><img class = "art-img" src = "Art/art (${i}).jpg"></div>`;
 }
 
-document.querySelector(".art-container").innerHTML = art
+grid.innerHTML = art
+
